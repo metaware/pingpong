36c9ee40b48c082f31cec6efbc0ecc8f3e023433
Jasdeep Singh <narang.jasdeep@gmail.com>
2017-03-09 14:02:05 -0500

    optimized queries on a few pages + added leaderboard table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,12 @@
 class HomeController < ApplicationController
+
   def index
+    # TODO: I'd likely add pagination here instead of blindly doing a .all
+    @users = User.all.includes(:games)
   end
 
   def history
-    @games = current_user.games
+    @games = current_user.games.includes(:player, :opponent)
   end
 
   def log
--- app/models/game.rb
+++ app/models/game.rb
@@ -17,25 +17,31 @@ class Game < ActiveRecord::Base
     opponent_score == player_score
   end
 
+  def player_scored_more?
+    player_score > opponent_score
+  end
+
+  def opponent_scored_more?
+    opponent_score > player_score
+  end
+
   def winner
-    if (player_score > opponent_score) && two_point_margin?
-      player
-    elsif (player_score < opponent_score) && two_point_margin?
-      opponent
-    end
+    return player if player_scored_more?
+    return opponent if opponent_scored_more?
   end
 
   def loser
-    return player if winner == player
-    return opponent if winner == opponent
+    return player if opponent_scored_more?
+    return opponent if player_scored_more?
   end
 
   def two_point_margin?
     (player_score - opponent_score).abs >= 2
   end
 
-  def outcome_for
-    # TODO
+  def outcome_for(user)
+    return :win if user == winner
+    return :loss if user == loser
   end
 
 end
